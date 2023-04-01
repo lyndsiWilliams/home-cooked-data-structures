cba36d091b6a9b6a15e0adc3d4faade73f89101b
Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
2023-04-01 16:06:13 -0500

    Update HomeCookedArray.js
    fill, find, findIndex, findLast, findLastIndex, flat in progress

--- HomeCookedArray.js
+++ HomeCookedArray.js
@@ -15,6 +15,9 @@ class HomeCookedArray {
     }
   }
 
+  // Returns the boundary prop if it exists, else returns a default value
+  _boundaryCheck = (boundary, defaultValue) => boundary ?? defaultValue;
+
   // Get an element at a specified index of the array
   at(index) {
     return this.data[index];
@@ -37,34 +40,90 @@ class HomeCookedArray {
     return newArray;
   }
 
-  // Checks whether all elements in the array pass the test passed into
-  // the function, returns a boolean value
+  /* Checks whether all elements in the array pass the test passed into
+    the function, returns a boolean value */
   every(checkFn) {
     let answer;
-    for (let i = 0; i < this.data.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       answer = checkFn(this.data[i]);
     }
     return answer;
   }
 
-  // Run a callback function on each element of the array
-  // Returns undefined
+  /* Changes all elements in an array to a static value, from a start index (default 0)
+    to an end index (default array.length). It returns the modified array. */
+  fill(value, start, end) {
+    for (
+      let i = this._boundaryCheck(start, 0);
+      i < this._boundaryCheck(end, this.length);
+      i++
+    ) {
+      this.data[i] = value;
+    }
+    return this.data;
+  }
+
+  /* Returns the first element in the provided array that satisfies the provided function.
+    If no values satisfy the testing function, undefined is returned. */
+  find(checkFn) {
+    for (let i = 0; i < this.length; i++) {
+      if (checkFn(this.data[i]) === true) return this.data[i];
+    }
+    return undefined;
+  }
+
+  /* Returns the index of the first element in the provided array that satisfies the
+    provided function. If no values satisfy the testing function, -1 is returned. */
+  findIndex(checkFn) {
+    for (let i = 0; i < this.length; i++) {
+      if (checkFn(this.data[i]) === true) return i;
+    }
+    return -1;
+  }
+
+  /* Returns the last element in the provided array that satisfies the provided function.
+    If no values satisfy the testing function, undefined is returned. */
+  findLast(checkFn) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (checkFn(this.data[i]) === true) return this.data[i];
+    }
+    return undefined;
+  }
+
+  /* Returns the index of the last element in the provided array that satisfies the
+    provided function. If no values satisfy the testing function, -1 is returned. */
+  findLastIndex(checkFn) {
+    for (let i = this.length - 1; i >= 0; i--) {
+      if (checkFn(this.data[i]) === true) return i;
+    }
+    return -1;
+  }
+
+  flat() {
+    for (let i = 0; i < this.length; i++) {
+      const stringyData = JSON.stringify(this.data[i]);
+      if (stringyData.includes("[")) console.log("array", stringyData);
+      console.log("not array", stringyData);
+    }
+  }
+
+  // Run a callback function on each element of the array, returns undefined
   forEach(callBackFn) {
-    for (let i = 0; i < this.data.length - 1; i++) {
+    for (let i = 0; i < this.length - 1; i++) {
       callBackFn(this.data[i]);
     }
   }
 
   // Check if the passed in element is in this.data
   includes(element) {
-    for (let i = 0; i < this.data.length - 1; i++) {
+    for (let i = 0; i < this.length - 1; i++) {
       if (this.data[i] == element) return true;
     }
     return false;
   }
 
-  // Run a callback function on each element of the array
-  // Returns a copy of the original array that's been mapped
+  /* Run a callback function on each element of the array
+    Returns a copy of the original array that's been mapped */
   map(callBackFn) {
     // Make a copy of the array's data
     const newArray = [...this.data];
@@ -122,3 +181,22 @@ console.log(
   "--- Every test:",
   numbers.every((currentValue) => currentValue < 40) // true
 );
+console.log("--- Fill test", numbers.fill(6, 2, 4));
+console.log(
+  "--- Find test",
+  numbers.find((element) => element > 10)
+);
+console.log(
+  "--- FindIndex test",
+  numbers.findIndex((element) => element > 10)
+);
+console.log(
+  "--- FindLast test",
+  numbers.findLast((element) => element > 10)
+);
+console.log(
+  "--- FindLastIndex test",
+  numbers.findLastIndex((element) => element > 10)
+);
+const notFlatNumbers = new HomeCookedArray([1, 2, [3, 4]]);
+console.log(notFlatNumbers.flat());
